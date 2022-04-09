9b3876b9ebb289118c4ae8e61a7638a4fe782a49
lauralbarracin <97710096+lauralbarracin@users.noreply.github.com>
2022-04-09 16:02:22 -0400

    Delete vars.py

--- vars.py
+++
@@ -1,7 +0,0 @@
-quizTotal = 0
-character = None
-
-characters = ["Laura (Logan`s Daughter)", "JessicaJones", "Wolverine","Deadpool"]
-
-#I am using Pillow package for the use of this images
-images = ["Laura.jpg", "JessicaJones.jpg", "Wolverine.jpg", "deadpool.jpg"]

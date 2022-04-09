bca0e6d5727f1e6912e2d7ed83027c0c3c42f828
lauralbarracin <97710096+lauralbarracin@users.noreply.github.com>
2022-04-09 15:56:39 -0400

    Add files via upload

---
+++ quizQuestions.py
@@ -0,0 +1,26 @@
+questions = {
+    "q1" : {
+        "question": "Are you female? ",
+        "yes" : 50,
+        "no" : 200
+    },
+
+    "q2" : {
+        "question" : "Are you of legal age? ",
+        "yes" : 0,
+        "no" : 100
+    },
+
+    "q3" : {
+        "question" : "are you wise-cracking? ",
+        "yes" : 300,
+        "no" : 0
+    },
+
+
+    "q4" : {
+        "question" : "Are you familiarized with adamntium? ",
+        "yes" : 150,
+        "no" : 0
+    },
+}
---
+++ vars.py
@@ -0,0 +1,7 @@
+quizTotal = 0
+character = None
+
+characters = ["Laura (Logan`s Daughter)", "JessicaJones", "Wolverine","Deadpool"]
+
+#I am using Pillow package for the use of this images
+images = ["Laura.jpg", "JessicaJones.jpg", "Wolverine.jpg", "deadpool.jpg"]
